f4080279b99ee5d8b126dd1db005c4733f2ac807
newjaehun <sjh030798@gmail.com>
2023-10-10 13:20:41 +0900

    BOJ1436

---
+++ algorithm/BOJ1436.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int count =0;
+        int result = 665;
+
+        while(count<n){
+            if(check(++result)){
+                count++;
+            }
+        }
+        System.out.println(result);
+        br.close();
+    }
+    public static boolean check(int n){
+        char[] arr = Integer.toString(n).toCharArray();
+        int count=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] =='6'){
+                count++;
+                if(count==3){
+                    return true;
+                }
+            }else{
+                count=0;
+            }
+        }
+        return false;
+//        return Integer.toString(n).contains("666");
+    }
+}

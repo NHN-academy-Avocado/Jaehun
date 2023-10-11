9709764128bff7586e5bd57c3bc506b410d537d7
newjaehun <sjh030798@gmail.com>
2023-10-11 10:33:45 +0900

    BOJ2751

---
+++ algorithm/BOJ2751.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BOJ2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> arr = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr.add(Integer.parseInt(br.readLine()));
+        }
+        br.close();
+        Collections.sort(arr);
+        for (int i = 0; i < n; i++) {
+            sb.append(arr.get(i) + "\n");
+        }
+        System.out.println(sb);
+    }
+}

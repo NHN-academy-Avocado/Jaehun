cc24a38f8cc5f29ad6ed7624762d01d8d5dfd63c
newjaehun <sjh030798@gmail.com>
2023-10-15 01:37:12 +0900

    Unit4 연습문제

---
+++ exercise/Introduction to Programming Using Java/Unit4/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+@
+src/Unit4_1.java,c/6/c6ad576bd6a14704c7ee205435bb22ceb3607264
+@
+src/Unit4_2.java,d/c/dc6c1e2db73fb652dccec1e0b48c289246a59c41
+@
+src/Unit4_3.java,9/a/9a3a4fd4e8bfe8080e2bd02079a4db360d9ada09
+@
+src/Unit4_4.java,9/0/90c6c27a8388c5a9adec8a5186df948508b59aa5
+G
+src/ArrayProcessor.java,4/5/45d2675b5a1547a5f32314e2189fa04949821fff
+@
+src/Unit4_5.java,e/f/efa6f29ef12390b4bdab3d1549d477bc633205d7
+@
+src/Unit4_8.java,b/8/b8769c79652c8af65ea13aa190e86ac059792fb0
--- exercise/Introduction to Programming Using Java/Unit4/src/Unit4_3.java
+++ exercise/Introduction to Programming Using Java/Unit4/src/Unit4_3.java
@@ -2,16 +2,20 @@ public class Unit4_3 {
     public static void main(String[] args) {
         System.out.println(dice(3));
     }
-    static int dice(int n){
-        int count = 0;
-        if(n<2 || n>12){
+
+    static int dice(int n) {
+        if (n < 2 || n > 12) {
             throw new IllegalArgumentException("error");
         }
-        int dice1 = (int)(Math.random()*6)+1;
-        int dice2 = (int)(Math.random()*6)+1;
-        while((dice1+dice2) !=n){
-            dice1 = (int)(Math.random()*6)+1;
-            dice2 = (int)(Math.random()*6)+1;
+        int dice1 = (int) (Math.random() * 6) + 1;
+        int dice2 = (int) (Math.random() * 6) + 1;
+        int count = 1;
+        if((dice1+dice2)==2){
+            return count;
+        }
+        while ((dice1 + dice2) != n) {
+            dice1 = (int) (Math.random() * 6) + 1;
+            dice2 = (int) (Math.random() * 6) + 1;
             count++;
         }
         return count;

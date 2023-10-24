86ccbc1277207ec80cb8749fe9b682cd37cb2508
newjaehun <sjh030798@gmail.com>
2023-10-25 00:03:30 +0900

    BOJ2108

---
+++ algorithm/class2/S3_BOJ2108.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class S3_BOJ2108 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] arr2 = new int[8001];
+
+
+        arr[0] = Integer.parseInt(br.readLine());
+        int sum = arr[0], min = arr[0], max = arr[0];
+        for (int i = 1; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        Arrays.sort(arr);
+
+        int a = (int) Math.round((double) sum / n);
+        int b = arr[n / 2];
+
+        int d = Math.abs(max - min);
+
+        int c = arr[0];
+        int count = 0;
+        max = -1;
+        boolean check = false;
+
+        //최빈값 구하는 반복문
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                count++;
+            } else {
+                count = 0;
+            }
+
+            if (max < count) {
+                max = count;
+                c = arr[i];
+                check = true;
+            } else if (max == count && check) {
+                c = arr[i];
+                check = false;
+            }
+        }
+
+
+        sb.append(a + "\n" + b + "\n" + c + "\n" + d + "\n");
+
+        System.out.println(sb);
+        br.close();
+    }
+}
